ee8bf6ee3792b55f29b422d2a648b1d671d48949
xujun <xujun@gegejia.com>
2019-08-13 23:27:15 +0800

    更新 dag

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -20,7 +20,6 @@ default_args = {
 dag = DAG('hello_world', default_args=default_args, schedule_interval=timedelta(minutes=1),
           start_date=datetime.now().replace(second=0, microsecond=0) - timedelta(minutes=1))
 
-# t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
     task_id='print_date',
     bash_command='date',
@@ -32,19 +31,4 @@ t2 = BashOperator(
     retries=3,
     dag=dag)
 
-templated_command = """
- { % f or i in range(5) %}
- echo "{{ ds }}"
- echo "{{ macros.ds_add(ds, 7)}}"
- echo "{{ params.my_param }}"
- { % e ndfor %}
-"""
-
-t3 = BashOperator(
-    task_id='templated',
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag)
-
 t2.set_upstream(t1)
-t3.set_upstream(t1)
